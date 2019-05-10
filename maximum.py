914f6a01a74c98d6fb4f7002f0d881d531e68c36
PraveenNellihela <43164013+PraveenNellihela@users.noreply.github.com>
2019-05-10 13:54:34 +0530

    Update maximum.py

--- maximum.py
+++ maximum.py
@@ -1,47 +1,45 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+#-------------- Import data ----------#
 data = np.genfromtxt("D:/Work/SLIIT/Humain gait/data/New Analysis/MAL/200 Steps/IMU Data/new_F1T1.csv", delimiter=',', skip_header=1, dtype=np.float)
-#print(data[:,6])                            # data [row,column], zero indexed  # extract entire column [:, x] #extract entire row [x,:]
-#print(data.shape)
+# data [row,column], zero indexed  # extract entire column [:, x] #extract entire row [x,:]
+#-------------------------------------#
 
+#------------- data setup ------------#
 gyro_z = data[:,6]
 time = data [:, 0]
-rows = data.shape[0]
-print('array size =',data.shape)
+rows = data.shape[0]    #no. of rows in array
+#-------------------------------------#
 
+#------- initialize numpy arrays -----#
 dif = np.zeros((rows, 1))
 max_number = np.zeros((rows, 1))
 maximum = np.zeros((rows,1))
-testT = np.zeros((rows,1))
-for i in range (0,rows-1,3):
+#-------------------------------------#
+
+for i in range (rows-2):
+    #--------- Differentiation -------#
     #v_dif = gyro_z[i+1]- gyro_z[i]
     #t_dif = time[i+1] - time[i]
     #dif[i] = v_dif/t_dif
+    #---------------------------------#
+
     #--------- Maximum ---------------#
     if gyro_z[i] < gyro_z[i+1] and gyro_z[i+1] > gyro_z[i+2] :
         max_number[i]= 1                 #iterable for total maximums
         maximum[i+1] = gyro_z[i+1]
-        testT[i+1] = time[i+1]
+    #---------------------------------#
 
-print(i)
-print(gyro_z[i])
-print(gyro_z[i+2])
-#print(dif)
 print('total number of maximums =',sum(max_number))
-print(maximum[i+1])
-#plt.plot(time, dif)
-#plt.show()
 
+#----------- Plot setup --------------#
 plt.plot(time,gyro_z, label = "original")
 plt.plot(time,maximum, label = "maximum")
-
 plt.xlabel('x - axis')
-# Set the y axis label of the current axis.
 plt.ylabel('y - axis')
-# Set a title of the current axes.
 plt.title('original vs. maximums ')
-# show a legend on the plot
 plt.legend()
-# Display a figure.
+#----------- Display plot ------------#
 plt.show()
+#-------------------------------------#
