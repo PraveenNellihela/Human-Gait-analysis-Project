314dba6a9dd6232716fbe1365b490435c1188ba7
Praveen Nellihela <praveennellihela@gmail.com>
2019-05-30 10:43:11 +0530

    This code can identify position b in human gait
    Maximas close to zero are recognized and marked in red on top of original data marked in blue.
    
    code made more streamlined

--- identify_pos_b_5_29_2019_mk1.py
+++ identify_pos_b_5_29_2019_mk1.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from bokeh.plotting import figure, output_file, show
+from bokeh.models import ColumnDataSource
 
 #-------------- Import data ----------#
 data = np.genfromtxt("D:/Work/SLIIT/Humain gait/data/New Analysis/MAL/200 Steps/IMU Data/new_F1T1.csv", delimiter=',', skip_header=1, dtype=np.float)
@@ -11,9 +12,10 @@ gyro_z = data[:,6]
 time = data [:, 0]
 rows = data.shape[0]        #no. of rows in array
 
-maximum = np.zeros((rows, 1))  # initialize maximum array
-pos_b = np.empty((rows, 1))  # initialize maximum array
-new_signal = np.empty((rows,1))
+maximum = list(np.zeros(rows)) # initialize maximum array
+new_signal = list(np.zeros(rows))
+
+
 #-------------------------------------#
 
 def plot_graph(file_name, title, x_label, y_label, legend,x_var, y_var):
@@ -26,17 +28,19 @@ def plot_graph(file_name, title, x_label, y_label, legend,x_var, y_var):
 
 def plot_multigraph(x1, y1, l1, c1, x2, y2, l2, c2,  x_label = 'x axis', y_label='y axis'):
     #--------------------------------- plot multiple graphs in one ---------------------------------------#
-    output_file("log_lines.html")                                       # output to static HTML file
+    output_file("multi_graph.html")                                       # output to static HTML file
                                                                         # create a new plot
     p = figure(
         tools="pan,box_zoom,reset,save",
         x_axis_label=x_label, y_axis_label=y_label
     )
-                                                                     # add some renderers
-    p.line(x1, y1, legend=l1)
-    p.circle(x1, y1, legend=l1, fill_color=c1, size=8)
+    #source = ColumnDataSource(new_signal)
 
-    p.line(x2, y2, legend=l2, line_color=c2)
+    # add some renderers
+    p.line(x1, y1, legend=l1)
+    p.circle(x1, y1, legend=l1, fill_color="white", size=8)
+
+    p.line(x2, y2, legend=l2, line_color=c2) #source=source)
     p.circle(x2, y2, legend=l2, fill_color=c2, line_color=c2, size=6)
                                                                         # show the results
     show(p)
@@ -48,7 +52,7 @@ def plot_original():
     #-----------------------------------------------------------------------------------------------------#
 
 def plot_LowPass(plot=False):
-    plot=plot
+    plot = plot
     #---------------------------------------- LowPass filter ---------------------------------------------#
     fc = 0.1
     b = 0.08
@@ -65,7 +69,7 @@ def plot_LowPass(plot=False):
     new_signal = np.convolve(s, sinc_func)
 
     if plot:
-        plot_graph(time, new_signal, 'lowpass filter', 'lowpass plot.html', '#FF6347')
+        plot_multigraph(time,new_signal,'lowpass filtered data','blue',time,maximum,'maximum','red','time','gyro data')
 
     #pos_b = new_signal[argrelextrema(new_signal, np.greater)[0]]
     #plot_graph(time, pos_b, 'position test', 'position test.html', '#C54C82')
@@ -73,28 +77,25 @@ def plot_LowPass(plot=False):
 
 def calculate_maximum(plot=False):
     #------------------------------------------ Maximum ---------------------------------------------------#
-    plot=plot
+    plot = plot
     for i in range(rows - 2):
         if gyro_z[i] <= gyro_z[i+1] >= gyro_z[i+2] and -10<=gyro_z[i+1]<=30:
             maximum[i+1] = gyro_z[i+1]
 
     if plot:
-        plot_graph("maximum.html","maximum plot",'time','maximum points', 'maximum', time, maximum[:,0] )
-        print(maximum)
-        #plot_multigraph(time, new_signal, maximum, 'combined plot', 'combined plot.html')
-        #plot_graph(time, maximum, 'maximum', 'maximum plot.html')
+        plot_multigraph(time, gyro_z, 'original data', 'blue', time, maximum, 'maximum', 'red', 'time',
+                        'gyro data')
+        #plot_graph("maximum.html","maximum plot",'time','maximum points', 'maximum', time, maximum[:,0] )
 
-    #pos_b = new_signal[argrelextrema(new_signal, np.greater)[0]]
-    #plot_graph(time, pos_b, 'position test', 'position test.html', '#C54C82')
     #-----------------------------------------------------------------------------------------------------#
 
 if __name__ == '__main__':
     #------------------------------------------ MAIN -----------------------------------------------------#
 
     plot_original()                                                     #plot original
-    #plot_LowPass()                                                     #plot lowpass
-    calculate_maximum(True)
+                                                        #plot lowpass
+    calculate_maximum()
+    plot_LowPass(True)
 
-    plot_multigraph(time,gyro_z,'original data','blue', time, maximum[:,0],'maximum','red','time', 'gyro data')
 
 
